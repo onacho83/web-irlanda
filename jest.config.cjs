2d1415c0b20300c0fc592e54eeb1a16f780c11c0
ignacioTech <ignaciobarboza1983@gmail.com>
2025-12-31 18:15:44 -0300

    test(jest): treat .js as ESM for tests (extensionsToTreatAsEsm)

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  transform: {}
+  transform: {},
+  // Tratar archivos .js como ESM para permitir 'import' en los tests
+  extensionsToTreatAsEsm: ['.js'],
+  testMatch: ['**/__tests__/**/*.test.js']
 };
