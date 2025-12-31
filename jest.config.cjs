3e89a159256b844875310a29516fcad90d44f65d
ignacioTech <ignaciobarboza1983@gmail.com>
2025-12-31 18:17:43 -0300

    test(jest): remove redundant extensionsToTreatAsEsm option

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,5 @@
 module.exports = {
   testEnvironment: 'jsdom',
   transform: {},
-  // Tratar archivos .js como ESM para permitir 'import' en los tests
-  extensionsToTreatAsEsm: ['.js'],
   testMatch: ['**/__tests__/**/*.test.js']
 };
